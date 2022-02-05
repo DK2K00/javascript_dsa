2fe8208a23e5ce578ce8930d051cac820a38d43b
Danush Kumar <danushkumar27@gmail.com>
2022-02-05 10:13:23 +0530

    Implemented push and pop for singly linked list

--- singly_linked_list/list.js
+++ singly_linked_list/list.js
@@ -1,18 +1,72 @@
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null;
-    }
+//Created a class for Node
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-class SingleyLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+//Class for singly linked list
+class SinglyLinkedList {
+  //Initialization
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  //Function to attach a new node at the end of the list
+  push(val) {
+    let newNode = new Node(val);
+    //Checking if list already exists
+    if (!this.head) {
+      //If not setting both head and tail as the new node
+      this.head = newNode;
+      this.tail = this.head;
+      //Else attaching the new node at the end
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length += 1;
+    console.log("Added: ", this);
+    return this;
+  }
+
+  pop() {
+    //Checking if list is empty
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    //Checking if list has only one element
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+      console.log(current);
+      return current;
+    }
+    //Variable to point to new tail
+    let newTail = current;
+
+    //Traversing through the list
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    //Setting the new tail by assigning the variable newTail
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length -= 1;
+
+    console.log("Popped", current);
+    return current;
+  }
 }
 
-let name = new Node("Danush");
-name.next = new Node("Kumar");
-name.next.next = new Node("Rocks");
+let list = new SinglyLinkedList();
+list.push("How's");
+list.push("it");
+list.push("going!");
+list.pop();
