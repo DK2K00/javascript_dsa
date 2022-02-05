8a2a18142966fa11ff25ea141c7d650ae21d54b8
Danush Kumar <danushkumar27@gmail.com>
2022-02-05 12:42:20 +0530

    Implemented shift and unshift

--- singly_linked_list/list.js
+++ singly_linked_list/list.js
@@ -63,10 +63,46 @@ class SinglyLinkedList {
     console.log("Popped", current);
     return current;
   }
+
+  shift() {
+    //Cehcking if list exists
+    if (!this.head) return undefined;
+    //Storing head value to return
+    let currentHead = this.head;
+    //Setting the next node as head
+    this.head = currentHead.next;
+    this.length -= 1;
+
+    //If list becomes empty, then setting tail as null
+    if (this.length === 0) this.tail = null;
+
+    console.log("Shifted value: ", currentHead);
+    return currentHead;
+  }
+
+  unshift(val) {
+    //Creating new node
+    let newNode = new Node(val);
+    //Checking if list is empty
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      //Else inserting node at the beginning and assigning it as head
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length += 1;
+    console.log("Unshifted node: ", this);
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
 list.push("How's");
 list.push("it");
-list.push("going!");
+list.push("going");
+list.push("Bro!!!");
 list.pop();
+list.shift();
+list.unshift("How's");
