af31127c3dc0d8acc9b070ca7174cb4f3fe7e252
Danush Kumar <danushkumar27@gmail.com>
2022-02-11 20:12:48 +0530

    Implemented push functionality

--- doubly_linked_list/list.js
+++ doubly_linked_list/list.js
@@ -12,4 +12,29 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    //Creating new node
+    let newNode = new Node(val);
+
+    //Checking if list is empty
+    //If so, set head and tail as the new node
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      //Else add after tail and set it as new tail
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    console.log(this);
+    return this;
+  }
 }
+
+let list = new DoublyLinkedList();
+list.push(10);
+list.push(20);
