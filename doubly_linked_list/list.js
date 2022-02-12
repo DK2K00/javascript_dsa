093ed43cff690a82402a77a2401fb07df9cac43c
Danush Kumar <danushkumar27@gmail.com>
2022-02-12 15:27:12 +0530

    Implemented shift and unshift functionalitites

--- doubly_linked_list/list.js
+++ doubly_linked_list/list.js
@@ -50,13 +50,57 @@ class DoublyLinkedList {
       this.tail.next = null;
       poppedNode.prev = null;
     }
-    this.length++;
+    this.length--;
     console.log("Pop: ", poppedNode);
     return poppedNode;
   }
+
+  //Function to remove element from beginning of list
+  shift() {
+    //Checking if empty
+    if (this.length === 0) return undefined;
+    let oldHead = this.head;
+
+    //Edge case
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      //Setting next element as head and removing previous elements
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    console.log("Shift: ", oldHead);
+    return oldHead;
+  }
+
+  //Function to add a node to the beginning of the list
+  unshift(val){
+    let newNode = new Node(val);
+    //If list is empty, setting head and tail as the new node
+    if(this.length === 0){
+      this.head = newNode;
+      this.tail = newNode;
+      //Else adding the new node and changing the pointers
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    console.log("Unshift:", newNode);
+    console.log("-------------------------------------");
+    console.log(this);
+    return this;
+  }
 }
+
 let list = new DoublyLinkedList();
 list.push(10);
 list.push(20);
 list.push(30);
 list.pop();
+list.unshift(0);
+list.shift();
