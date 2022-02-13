88be84a31e5561800c8339529f375e9988c206b0
Danush Kumar <danushkumar27@gmail.com>
2022-02-13 12:22:32 +0530

    Implemented insert and remove functionalities

--- doubly_linked_list/list.js
+++ doubly_linked_list/list.js
@@ -99,10 +99,10 @@ class DoublyLinkedList {
   //Function to get value of a particular index
   get(index) {
     //Edge case
-    if (index < 0 || index >= this.length){
+    if (index < 0 || index >= this.length) {
       console.log("Invalid index for get");
       return null;
-    } 
+    }
     let count, current;
 
     //Checking if index is closer to head or tail
@@ -129,18 +129,84 @@ class DoublyLinkedList {
   }
 
   //Function to change value for particular index
-  set(index,val){
+  set(index, val) {
     //Getting the node
     let foundNode = this.get(index);
     //Changing value if found
-    if(foundNode != null){
+    if (foundNode != null) {
       foundNode.val = val;
-      console.log("Set: ",foundNode);
+      console.log("Set: ", foundNode);
       return true;
     }
     //Returning false if not found
     console.log("Node not found for set");
     return false;
+  }
+
+  //Function to insert a node in the list
+  insert(index, val) {
+    //Edge cases
+    if (index < 0 || index >= this.length) {
+      console.log("Invalid index for insert");
+      return false;
+    }
+    if (index === 0) {
+      console.log("Node is added at the beginning");
+      return !!this.unshift(val);
+    }
+    if (index === this.length - 1) {
+      console.log("Node is added at the end");
+      return !!this.push(val);
+    }
+
+    //Creating new node
+    let newNode = new Node(val);
+    //Getting previous and next node of given index
+    let beforeNode = this.get(index - 1);
+    let afterNode = beforeNode.next;
+
+    //Changing pointers
+    newNode.prev = beforeNode;
+    beforeNode.next = newNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+
+    this.length++;
+    console.log("Insert: ", newNode);
+    return true;
+  }
+
+  //Function to remove node from list
+  remove(index) {
+    //Edge cases
+    if (index < 0 || index >= this.length) {
+      console.log("Invalid index for remove");
+      return undefined;
+    }
+    if (index === 0) {
+      console.log("Node is removed from the beginning");
+      return this.shift();
+    }
+    if (index === this.length - 1) {
+      console.log("Node is removed from the end");
+      return this.pop();
+    }
+
+    //Getting node to be removed
+    let removedNode = this.get(index);
+    //Getting previous and next node
+    let beforeNode = removedNode.prev;
+    let afterNode = removedNode.next;
+
+    //Changing pointers
+    beforeNode.next = afterNode;
+    afterNode.prev = beforeNode;
+    removedNode.next = null;
+    removedNode.prev = null;
+
+    this.length--;
+    console.log("Remove: ", removedNode);
+    return removedNode;
   }
 }
 
@@ -155,5 +221,8 @@ list.push(40);
 list.push(50);
 list.push(60);
 list.get(3);
-list.set(2,30);
-list.set(-1,0);
+list.set(2, 30);
+list.set(-1, 0);
+list.insert(3, 40);
+list.insert(-1, 0);
+list.remove(5);
