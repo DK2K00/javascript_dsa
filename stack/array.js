771faf27226b28941955dbf2da09046efe386e93
Danush Kumar <danushkumar27@gmail.com>
2022-03-30 11:14:19 +0530

    Array implementation of stack

---
+++ stack/array.js
@@ -0,0 +1,13 @@
+//Creating stack using array
+let stack = [];
+
+//Pushing elements into the stack
+stack.push("hello");
+stack.push("there");
+stack.push("user");
+
+console.log(stack);
+
+//Removing top most element of stack(LIFO)
+stack.pop();
+console.log(stack);
