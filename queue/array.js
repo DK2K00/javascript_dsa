4164d2502511d444398230a3ed6baa6a034b842e
Danush Kumar <danushkumar27@gmail.com>
2022-04-01 10:59:47 +0530

    Array and linked list implementation of queue

---
+++ queue/array.js
@@ -0,0 +1,12 @@
+let queue = [];
+
+//Pushing into the array
+queue.push("hello");
+queue.push("there");
+queue.push("everyone");
+
+console.log(queue);
+
+//Taking out the first element which is pushed
+queue.shift();
+console.log(queue);
